09f7af996ffc27fc6fabe0c4ad15143bbe33d8e4
fes <fesss32@yandex.ru>
2020-12-13 22:30:28 +0500

    Init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Time.deltaTime*_speed,0,0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Time.deltaTime * _speed, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, Time.deltaTime * _speed,  0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -Time.deltaTime * _speed, 0);
+        }     
+    }
+}
---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    private bool _isMaxVolumeReached = false;
+    private bool _isAlarm = false;
+
+    private const float _minVolume = 0;
+    private const float _maxVolume = 1;
+
+    private void OnTriggerEnter2D(Collider2D    collision)
+    {
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            if (_isAlarm)
+            {
+                DeactivateAlarm();
+            }
+            else
+            {
+                ActivateAlarm();
+            }
+        }
+    }
+
+    private void ActivateAlarm()
+    {
+        _isAlarm = true;
+        StartCoroutine(AlarmSignaling());
+    }
+
+    private void DeactivateAlarm()
+    {
+        _audioSource.Stop();
+        _isAlarm = false;
+    }
+
+    private IEnumerator AlarmSignaling()
+    {
+        _audioSource.UnPause();
+        _audioSource.Play();
+
+        while (_isAlarm)
+        {
+            if (!_isMaxVolumeReached)
+            {
+                for (float i = 0; i <= _maxVolume; i += 0.001f)
+                {
+                    _audioSource.volume = _minVolume + i;
+                    yield return null;
+                }
+                _isMaxVolumeReached = true;
+            }
+            else
+            {
+                for (float i = _audioSource.volume; i >= 0; i -= 0.001f)
+                {
+                    _audioSource.volume = i;
+                    yield return null;
+                }
+                _isMaxVolumeReached = false;
+            }
+        }
+    }
+}
